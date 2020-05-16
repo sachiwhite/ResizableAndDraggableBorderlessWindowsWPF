2ab2849fb4fb12c0b8e29dde7c53a545aaffeaed
Blanka Lewonowska <loczkovata@gmail.com>
2020-05-16 21:30:44 +0200

    added: closing, maximizing, minimizing and dragging window

---
+++ ResizableBorderlessLib/BorderControlViewModel.cs
@@ -0,0 +1,43 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ResizableBorderlessLib
+{
+    public class BorderControlViewModel :ViewModelBase
+    {
+        public BorderControlViewModel()
+        {
+            DragWindowCommand=new RelayCommand(DragWindow);
+            CloseWindowCommand=new RelayCommand(CloseWindow);
+            MaximizeWindowCommand=new RelayCommand(MaximizeWindow);
+            MinimizeWindowCommand=new RelayCommand(MinimizeWindow);
+          
+        }
+
+        private void MaximizeWindow()
+        {
+            WindowState windowState = Application.Current.MainWindow.WindowState;
+            if (windowState == WindowState.Normal)  
+                Application.Current.MainWindow.WindowState=WindowState.Maximized;
+            else Application.Current.MainWindow.WindowState=WindowState.Normal;
+
+        }
+
+        public ImageSource IconSource => Application.Current.MainWindow.Icon;
+        private void DragWindow() => Application.Current.MainWindow.DragMove();
+        public string BorderLabel => Application.Current.MainWindow.Title;
+        public RelayCommand DragWindowCommand { get;}
+        public RelayCommand CloseWindowCommand{get;}
+        public RelayCommand MaximizeWindowCommand{get;}
+        public RelayCommand MinimizeWindowCommand{get;}
+        public void MinimizeWindow() => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        public void CloseWindow() => Application.Current.MainWindow.Close();
+    }
+}
